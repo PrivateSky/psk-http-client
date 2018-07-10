bb53dcf55fa21877d902bb401012ce46a709591e
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2018-07-10 11:46:34 +0300

    first API sketch

---
+++ psk-http-client.js
@@ -0,0 +1,20 @@
+function PskHttpClient(remoteEndPoint, agentUid, cryptoInfo){
+    this.startSwarm = function(swarmName, phase, ...args){
+
+    }
+
+    function Request(){
+        this.on = function(returnPhaseName, callback){}
+        this.onReturn = function(callback){}
+    }
+
+    this.upload = function(cryptoUid, binaryData, callback){
+
+    }
+
+    this.download = function(cryptoUid, callback){
+
+    }
+
+
+}
